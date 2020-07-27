ef28ed1b7c6701348e1901daca2c7daa6424a247
Andrea <miranda.andrea.nd@gmail.com>
2020-07-27 17:44:13 +0200

    aggiunta ajax(approssimata)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 console.log("heyyy!!!!");
+
+
+  function faiCose() {
+    $.ajax(
+      {
+        url: "https://api.themoviedb.org/3/movie/550",
+        method: "GET",
+        success: function (data, stato) {
+          $("#risultati").html(data);
+        },
+        error: function (richiesta, stato, errori) {
+          alert("E' avvenuto un errore. " + errore);
+        }
+      }
+    );
+  }
+  console.log(faiCose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init() {
+
+}
