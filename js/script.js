e69e47958a58afc00a95c4291a5d776fba5d6630
Andrea <miranda.andrea.nd@gmail.com>
2020-07-28 13:00:36 +0200

    rifatto la prima parte(ma non funziona la url)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
-console.log("heyyy!!!!");
+function addSearchClick() {
+
+var button = $("#button-text");
+button.click(getMovies);
+
+}
 
 
-  function faiCose() {
-    $.ajax(
-      {
-        url: "https://api.themoviedb.org/3/movie/550",
-        method: "GET",
-        success: function (data, stato) {
-          $("#risultati").html(data);
-        },
-        error: function (richiesta, stato, errori) {
-          alert("E' avvenuto un errore. " + errore);
-        }
-      }
-    );
-  }
-  console.log(faiCose);
+function getMovies() {
 
+  var target = $("#input-text");
+  var query = target.val();
 
+  $.ajax ({
 
+    url:"https://api.themoviedb.org/3/search/movie",
+    method:"GET",
+    data: {
+      "api_key" : "6f82cb1652a4158cd02147297fe88ff0",
+      "query" : query
+    },
 
+    succes: function(data) {
+      console.log(data);
+    },
 
+    error: function(error){
+      console.log(error);
+    }
 
+  });
 
-
-
-
-
-
-
-
+}
 
 function init() {
 
+  addSearchClick();
+
 }
+
+$(document).ready(init);
